cc1af5f712c28f815e480411c4b58ecccf46e197
Cashiuus <cashiuus@gmail.com>
2017-03-14 22:19:32 -0400

    Added dir check for usr local

--- system-setup/kali/setup-pycharm.sh
+++ system-setup/kali/setup-pycharm.sh
@@ -46,7 +46,7 @@ APP_SETTINGS="${HOME}/.config/penbuilder/settings.conf"
 APP_ARGS=$@
 DEBUG=true
 LOG_FILE="${APP_BASE}/debug.log"
-
+BACKUPS_PATH="${HOME}/Backups"
 # TODO: Get latest pycharm version programmatically
 PYCHARM_VERSION="2016.3.2"
 
@@ -78,20 +78,21 @@ fi
 
 
 # Save our preferences/settings before getting rid of old versions
-[[ ! -d ${HOME}/Backups ]] && mkdir -p ${HOME}/Backups
+echo -e "${GREEN}[*] ${RESET}Checking for previous versions to backup settings"
+[[ ! -d "${BACKUPS_PATH}" ]] && mkdir -p "${BACKUPS_PATH}"
 if [[ -d ${HOME}/.PyCharm2016.2 ]]; then
-    cp ${HOME}/.PyCharm2016.2/config/settings.jar ${HOME}/Backups/
+    cp ${HOME}/.PyCharm2016.2/config/settings.jar "${BACKUPS_PATH}/"
 elif [[ -d ${HOME}/.PyCharm2016.3 ]]; then
-    cp ${HOME}/.PyCharmCE2016.3/config/settings.jar ${HOME}/Backups/
+    cp ${HOME}/.PyCharmCE2016.3/config/settings.jar "${BACKUPS_PATH}/"
 fi
 
 # https://confluence.jetbrains.com/display/PYH/Previous+PyCharm+Releases
 # https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=linux
+echo -e "${GREEN}[*] ${RESET}Downloading PyCharm package, please wait..."
 cd /tmp
 wget --no-verbose http://download.jetbrains.com/python/pycharm-community-${PYCHARM_VERSION}.tar.gz
 #wget -q https://download-cf.jetbrains.com/python/pycharm-community-${PYCHARM_VERSION}.tar.gz
 tar xzf pycharm-community-${PYCHARM_VERSION}.tar.gz
-
 rm /tmp/pycharm-community-${PYCHARM_VERSION}.tar.gz
 
 # TODO: Can we simply overwrite the files, or should we delete the old folder first?
@@ -102,6 +103,8 @@ mv /tmp/pycharm-community* /opt/pycharm-community
 
 
 # Create permanent symlinks so we can easily open pycharm going forward
+echo -e "${GREEN}[*] ${RESET}Creating symlinks for PyCharm"
+[[ ! -d /usr/local/bin ]] && mkdir -p /usr/local/bin
 [[ ! -f /usr/local/bin/pycharm ]] && ln -s /opt/pycharm-community/bin/pycharm.sh /usr/local/bin/pycharm
 [[ ! -f /usr/local/bin/inspect ]] && ln -s /opt/pycharm-community/bin/inspect.sh /usr/local/bin/inspect
 
@@ -111,7 +114,7 @@ mv /tmp/pycharm-community* /opt/pycharm-community
 #Server key = http://idea.qinxi1992.cn
 
 # Copy the PyCharm Icon over to our default applications pool
-cp /opt/pycharm-community/bin/pycharm.png /usr/share/applications/
+###cp /opt/pycharm-community/bin/pycharm.png /usr/share/applications/
 
 # Setup desktop icon if you wish
 file="${HOME}/Desktop/pycharm.desktop"
@@ -128,70 +131,15 @@ Terminal=false
 Type=Application
 EOF
 fi
-chmod +x "${file}"
+chmod 0500 "${file}"
 
 
 function finish {
     # Any script-termination routines go here, but function cannot be empty
     #clear
-    echo -e "${GREEN}[$(date +"%F %T")] ${RESET}Installer shutting down, please wait..." | tee -a "${LOG_FILE}"
+    echo -e "${GREEN}[$(date +"%F %T")] ${RESET}PyCharm Installer complete, goodbye!"
     # Redirect app output to log, sending both stdout and stderr (*NOTE: this will not parse color codes)
     # cmd_here 2>&1 | tee -a "${LOG_FILE}"
 }
 # End of script
 trap finish EXIT
-
-## ========================================================================== ##
-## =====================[ Template File Syntax Help ]======================== ##
-
-# Run echo and cat commands through sudo (notice the single quotes)
-# sudo sh -c 'echo "strings here" >> /path/to/file'
-
-
-# =========[ Expression Cheat Sheet ]=========
-#
-#   -d      file exists and is a directory
-#   -e      file exists
-#   -f      file exists and is a regular file
-#   -h      file exists and is a symbolic link
-#   -s      file exists and size greater than zero
-#   -r      file exists and has read permission
-#   -w      file exists and write permission granted
-#   -x      file exists and execute permission granted
-#   -z      file is size zero (empty)
-#
-#   $#      Number of arguments passed to script by user
-#   $@      A list of available parameters (*avoid using this)
-#   $?      The exit value of the command run before requesting this
-#   $0      Name of the running script
-#   $1..5   Arguments given to script by user
-#   $$      Process ID of the currently-running shell the script is running in
-#
-#   [[ $? -eq 0 ]]      Previous command was successful
-#   [[ $? -ne 0 ]]      Previous command NOT successful
-#
-#
-# ====[ READ ]==== #
-#   -p ""   Instead of echoing text, provide it right in the "prompt" argument
-#               *NOTE: Typically, there is no newline, so you may need to follow
-#               this with an "echo" statement to output a newline.
-#   -e      Specify variable response is stored in. Arg can be anywhere,
-#           but variable is always at the end of the statement
-#   -n #    Number of seconds to wait for a response before continuing automatically
-#   -i ""   Specify a default value. If user hits ENTER or doesn't respond, this value is saved
-#
-# Ask for a path with a default value
-#   read -n 5 -p "Enter the path to the file: " -i "/usr/local/etc/" -e FILEPATH
-#   echo -e ""
-
-# ====[ TOUCH ]==== #
-#touch
-#touch "$file" 2>/dev/null || { echo "Cannot write to $file" >&2; exit 1; }
-
-# ====[ SED ]==== #
-#sed -i 's/^.*editor_font=.*/editor_font=Monospace\ 10/' "${file}"
-#sed -i 's|^.*editor_font=.*|editor_font=Monospace\ 10|' "${file}"
-
-# ==================[ BASH GUIDES ]====================== #
-# Using Exit Codes: http://bencane.com/2014/09/02/understanding-exit-codes-and-how-to-use-them-in-bash-scripts/
-# Writing Robust BASH Scripts: http://www.davidpashley.com/articles/writing-robust-shell-scripts/
