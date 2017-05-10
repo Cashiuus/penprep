9ded4d1724107e47a091f84f9db6b8472046c00d
Cashiuus <cashiuus@gmail.com>
2017-05-10 16:14:54 -0400

    xfce tweaks

--- system-setup/debian/setup-debian.sh
+++ system-setup/debian/setup-debian.sh
@@ -1,40 +1,30 @@
 #!/usr/bin/env bash
 ## =======================================================================================
 # File:     setup-debian.sh
-#
 # Author:   Cashiuus
-# Created:  15-Jan-2016         Revised:        31-Mar-2017
+# Created:  15-Jan-2016         Revised:  10-May-2017
 #
 #-[ Info ]-------------------------------------------------------------------------------
-# Purpose:  Setup a fresh Debian 8 server, typically within a Virtual Machine.
+#- Purpose:  Setup a fresh Debian 8 server, typically within a Virtual Machine.
 #
 #
-#-[ Notes ]-------------------------------------------------------------------------------
-#
-#       1.      Below, set the constant "INSTALL_USER" to your primary account you are using
-#               If you don't, it'll default to 'user1'
+#- Notes:
+#     1.  Below, set the constant "INSTALL_USER" to your primary account you are using
+#         If you don't, it'll default to 'user1'
 #
 #
 #-[ Links/Credit ]------------------------------------------------------------------------
+#   - http://www.debiantutorials.com/
+#   - Help: http://www.pontikis.net/blog/debian-wheezy-web-server-setup
+#   - Tutorial: https://www.digitalocean.com/community/tutorials/initial-server-setup-with-debian-8
 #
-# - http://www.debiantutorials.com/
-# - Help: http://www.pontikis.net/blog/debian-wheezy-web-server-setup
-# - Tutorial: https://www.digitalocean.com/community/tutorials/initial-server-setup-with-debian-8
 #
 #-[ Copyright ]---------------------------------------------------------------------------
 #   MIT License ~ http://opensource.org/licenses/MIT
 ## =======================================================================================
-__version__="1.1"
+__version__="1.2"
 __author__="Cashiuus"
-## ========[ TEXT COLORS ]=============== ##
-GREEN="\033[01;32m"     # Success
-YELLOW="\033[01;33m"    # Warnings/Information
-RED="\033[01;31m"       # Issues/Errors
-BLUE="\033[01;34m"      # Heading
-PURPLE="\033[01;35m"    # Other
-ORANGE="\033[38;5;208m" # Debugging
-BOLD="\033[01;01m"      # Highlight
-RESET="\033[00m"        # Normal
+
 ## ============[ CONSTANTS ]================ ##
 START_TIME=$(date +%s)
 APP_PATH=$(readlink -f $0)          # Previously "${SCRIPT_DIR}"
@@ -44,49 +34,40 @@ APP_SETTINGS="${HOME}/.config/penbuilder/settings.conf"
 
 ## ============[ CONSTANTS ]================ ##
 
+DEBIAN_VERSION=$(lsb_release -cs)
+
 ## =========================[ START :: LOAD FILES ]========================= ##
 if [[ -s "${APP_BASE}/../common.sh" ]]; then
-    source "${APP_BASE}/../common.sh"
-    [[ "$DEBUG" = true ]] && echo -e "${ORANGE}[DEBUG] :: source files :: success${RESET}"
+  source "${APP_BASE}/../common.sh"
+  [[ "$DEBUG" = true ]] && echo -e "${ORANGE}[DEBUG] :: source files :: success${RESET}"
 else
-    echo -e "${RED} [ERROR]${RESET} common.sh functions file is missing."
-    [[ "$DEBUG" = true ]] && echo -e "${ORANGE}[DEBUG] :: source files :: fail${RESET}"
-    exit 1
+  echo -e "${RED} [ERROR]${RESET} common.sh functions file is missing."
+  [[ "$DEBUG" = true ]] && echo -e "${ORANGE}[DEBUG] :: source files :: fail${RESET}"
+  exit 1
 fi
 ## ==========================[ END :: LOAD FILES ]]========================== ##
 
 # Check root/sudo rights and fix it before continuing
 check_root
 
-
+echo -e "${GREEN}[*]${RESET} Debian Release: ${ORANGE}$(lsb_release -cs)${RESET}"
+# Try update first, because much of the script is useless without networking.
+$SUDO apt-get -qq update
+if [[ "$?" -ne 0 ]]; then
+  echo -e "${RED} [ERROR]${RESET} Network issues preventing apt-get. Check and try again."
+  exit 1
+fi
 ## ========================================================================== ##
-function configure_bash_systemwide() {
-        echo -e "[*] Configuring systemwide BASH history settings"
-    file=/etc/bash.bashrc
-
-    grep -q "HISTSIZE" "${file}" \
-        || echo "HISTSIZE=10000" >> "${file}"               # Bash history (memory scroll back)
-    sed -i 's/^HISTSIZE=.*/HISTSIZE=10000/' "${file}"
-    grep -q "HISTFILESIZE" "${file}" \
-        || echo "HISTFILESIZE=10000" >> "${file}"           # Bash history (file .bash_history)
-    sed -i 's/^HISTFILESIZE=.*/HISTFILESIZE=10000/' "${file}"
-
-    # If last line of file is not blank, add a blank spacer line
-    ([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
-    sed -i 's/.*force_color_prompt=.*/force_color_prompt=yes/' "${file}"
-    grep -q '^force_color_prompt' "${file}" 2>/dev/null \
-      || echo 'force_color_prompt=yes' >> "${file}"
-    sed -i 's#PS1='"'"'.*'"'"'#PS1='"'"'${debian_chroot:+($debian_chroot)}\\[\\033\[01;31m\\]\\u@\\h\\\[\\033\[00m\\]:\\[\\033\[01;34m\\]\\w\\[\\033\[00m\\]\\$ '"'"'#' "${file}"
-    grep -q "^export LS_OPTIONS='--color=auto'" "${file}" 2>/dev/null \
-      || echo "export LS_OPTIONS='--color=auto'" >> "${file}"
-
-    # All other users that are made afterwards
-    file=/etc/skel/.bashrc
-    sed -i 's/.*force_color_prompt=.*/force_color_prompt=yes/' "${file}"
+function pause() {
+  # Simple function to pause a script mid-stride
+  #
+  local dummy
+  read -s -r -p "Press any key to continue..." -n 1 dummy
+  echo -e ""
 }
+
 # ================================[  BEGIN  ]================================ #
-echo -e ""
-# =========================[ Setup VM Tools ]============================ #
+# -==  Setup VM Tools  ==- #
 # https://github.com/vmware/open-vm-tools
 if [[ ! $(which vmware-toolbox-cmd) ]]; then
   echo -e "${YELLOW}[-] Now installing vm-tools. This will require a reboot. Re-run script after...${RESET}"
@@ -95,53 +76,78 @@ if [[ ! $(which vmware-toolbox-cmd) ]]; then
   $SUDO reboot
 fi
 
-# =============================[   APT   ]================================ #
+# Increase idle delay which locks the screen (default is 300s)
+$SUDO gsettings set org.gnome.desktop.session idle-delay 0
+
+# -==   APT   ==- #
 # https://wiki.debian.org/SourcesList
-echo -e "\n${GREEN}[*]${RESET} Setting sources.list to standard entries"
-if [[ $SUDO ]]; then
-  echo "# Debian Jessie" | $SUDO tee /etc/apt/sources.list
-  echo "deb http://httpredir.debian.org/debian jessie main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
-  echo "deb-src http://httpredir.debian.org/debian jessie main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+# Only modify the sources.list if we are on a 'jessie' system, else stick with defaults
+if [[ ${DEBIAN_VERSION} == 'jessie' ]]; then
+  echo -e "\n${GREEN}[*]${RESET} Setting sources.list to standard entries"
+  if [[ $SUDO ]]; then
+    echo "# Debian Jessie" | $SUDO tee /etc/apt/sources.list
+    echo "deb http://httpredir.debian.org/debian jessie main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+    echo "deb-src http://httpredir.debian.org/debian jessie main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
 
-  echo "deb http://httpredir.debian.org/debian jessie-updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
-  echo "deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+    echo "deb http://httpredir.debian.org/debian jessie-updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+    echo "deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
 
-  echo "deb http://security.debian.org/ jessie/updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
-  echo "deb-src http://security.debian.org/ jessie/updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
-        # Backports repo for newer pkgs
-        echo "" | $SUDO tee -a /etc/apt/sources.list
-        echo "# Backports Repository" | $SUDO tee -a /etc/apt/sources.list
-        echo "deb http://httpredir.debian.org/debian jessie-backports main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
-else
-  echo "# Debian Jessie" > /etc/apt/sources.list
-  echo "deb http://httpredir.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list
-  echo "deb-src http://httpredir.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list
+    echo "deb http://security.debian.org/ jessie/updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+    echo "deb-src http://security.debian.org/ jessie/updates main contrib non-free" | $SUDO tee -a /etc/apt/sources.list
+  else
+    echo "# Debian Jessie" > /etc/apt/sources.list
+    echo "deb http://httpredir.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list
+    echo "deb-src http://httpredir.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list
 
-  echo "deb http://httpredir.debian.org/debian jessie-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free" >> /etc/apt/sources.list
+    echo "deb http://httpredir.debian.org/debian jessie-updates main contrib non-free" >> /etc/apt/sources.list
+    echo "deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free" >> /etc/apt/sources.list
 
-  echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb-src http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
-        # Backports repo for newer pkgs
-        echo "" >> /etc/apt/sources.list
-        echo "# Backports Repository" >> /etc/apt/sources.list
-        echo "deb http://httpredir.debian.org/debian jessie-backports main contrib non-free" >> /etc/apt/sources.list
+    echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
+    echo "deb-src http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list
+  fi
 fi
-
 
 echo -e "${GREEN}[*]${RESET} Performing apt-get update, please wait..."
 export DEBIAN_FRONTEND=noninteractive
 $SUDO apt-get -qq update
-if [[ "$?" -ne 0 ]]; then
-  echo -e "${RED} [ERROR]${RESET} Network issues preventing apt-get. Check and try again."
-  exit 1
+
+# -== Gnome & XFCE default tweaks ==- #
+# xfce4-settings-editor will show settings in GUI window
+
+
+# Configure XFCE4 Terminal defaults in its config file
+file="${HOME}/.config/xfce4/terminal/terminalrc"
+if [[ -s "${file}" ]]; then
+  # TODO: Not sure if this file is present on a vanilla install and/or if
+  # these 'scrolling' settings were only present because I had tweaked them.
+  sed -i 's/^ScrollingLines=.*/ScrollingLines=9000/' "${file}"
+  sed -i 's/^ScrollingOnOutput=.*/ScrollingOnOutput=FALSE/' "${file}"
+  echo "FontName=Monospace 11" >> "${file}"
+  echo "BackgroundMode=TERMINAL_BACKGROUND_TRANSPARENT" >> "${file}"
+  echo "BackgroundDarkness=0.970000" >> "${file}"
 fi
 
-# Increase idle delay which locks the screen (default is 300s)
-$SUDO gsettings set org.gnome.desktop.session idle-delay 0
+# Setup 3 workspaces (Default: 4)
+xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 3
+
+function xfce_setup_thunar {
+  # Thunar file explorer (need to re-login for effect)
+  # xfconf-query apparently doesn't require sudo, settings must be user-scope stored
+  xfconf-query -n -c thunar -p /last-show-hidden -t bool -s true
+  xfconf-query -n -c thunar -p /last-details-view-column-widths -t string -s "50,133,50,50,178,50,50,73,70"
+  xfconf-query -n -c thunar -p /last-view -t string -s "ThunarDetailsView"
+
+  mkdir -p ~/.config/Thunar/
+  file=~/.config/Thunar/thunarrc
+  ([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+  sed -i 's/LastShowHidden=.*/LastShowHidden=TRUE/' "${file}" 2>/dev/null \
+    || echo -e "[Configuration]\nLastShowHidden=TRUE" > "${file}"
+}
+xfce_setup_thunar
+
+
 #$SUDO gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 0
 #$SUDO gsettings set org.gnome.settings-daemon.plugins.power sleep-display-battery 0
-
 
 # Launch xscreensaver settings to auto-generate the config file we need to edit
 echo -e "${GREEN}[*]${RESET} Launching and fixing ${GREEN}xscreensaver${RESET}, please wait..."
@@ -166,19 +172,22 @@ $SUDO apt-get -y install openvpn openssl openssh-server
 # baobab = Disk Usage Analyzer - Menu shortcut will show up under Applications -> SYSTEM
 $SUDO apt-get -y install baobab
 
-# Initializing them disabled to prevent insecure remote ssh sever to be cautious
+# Initializing them disabled to prevent insecure remote services to be cautious
 $SUDO systemctl stop ssh.service
 $SUDO systemctl disable ssh.service
 
 $SUDO systemctl stop exim4.service
 $SUDO systemctl disable exim4.service
 
+$SUDO systemctl stop apache2.service
+$SUDO systemctl disable apache2.service
+
 # ====[ Remove Bloat ]======
 #$SUDO apt-get -y remove libreoffice libreoffice-base
 #$SUDO apt-get -y autoremove
 
 
-# Create desktop shortcuts -- just delete if you don't want them
+# ====[ Create desktop shortcuts -- just delete if you don't want them
 cp /usr/share/applications/xfce4-terminal.desktop ~/Desktop/xfce4-terminal.desktop
 cp /usr/share/applications/firefox-esr.desktop ~/Desktop/
 cp /usr/share/applications/geany.desktop ~/Desktop/geany.desktop
@@ -188,50 +197,52 @@ chmod +x ~/Desktop/geany.desktop
 
 
 
-# =============[ CONKY via Backports Repo ]===============
+## -============[ Debian Jessie Backports Repository ]=============- #
+file=/etc/apt/sources.list.d/backports.list
+$SUDO sh -c "echo ### Debian Jessie Backports > ${file}"
+$SUDO sh -c "echo deb http://httpredir.debian.org/debian jessie-backports main contrib non-free >> ${file}"
+
 # This is how you can see a list of all installed backports:
-#       dpkg-query -W | grep ~bpo
+#   dpkg-query -W | grep ~bpo
+# View list of all potential packages:
+#   apt-cache policy <pkg>
 
 echo -e "${GREEN}[*]${RESET} Installing Conky pkg..."
 $SUDO apt-get -y -t jessie-backports install conky
 
 
 
+# -== Git global config settings ==- #
+echo -e "${YELLOW}[INPUT]${RESET} Git global config :: Enter your name: "
+read $RES
+git config --global user.name $RES
+echo -e "${YELLOW}[INPUT]${RESET} Git global config :: Enter your email: "
+read $RES
+git config --global user.email $RES
+git config --global color.ui auto
 
-
-
-
-
-
-
-function pause() {
-  # Simple function to pause a script mid-stride
-  #
-  local dummy
-  read -s -r -p "Press any key to continue..." -n 1 dummy
-}
 
 
 function finish() {
-        # Clean system
-        echo -e "\n\n${GREEN}[*] ${RESET}Cleaning up the aptitude pkg system"
-        $SUDO apt-get -y -qq clean && $SUDO apt-get -y -qq autoremove
-        # Remove purged packages from system
-        $SUDO apt-get -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')
-        cd ~/ &>/dev/null
-        history -c 2>/dev/null
+  # Clean system
+  echo -e "\n\n${GREEN}[*] ${RESET}Cleaning up the aptitude pkg system"
+  $SUDO apt-get -y -qq clean && $SUDO apt-get -y -qq autoremove
+  # Remove purged packages from system
+  $SUDO apt-get -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')
+  cd ~/ &>/dev/null
+  history -c 2>/dev/null
 
-        echo -e "\n\n${GREEN}[*] ${RESET}Updating the locate database"
-        $SUDO updatedb
+  echo -e "\n\n${GREEN}[*] ${RESET}Updating the locate database"
+  $SUDO updatedb
 
-        FINISH_TIME=$(date +%s)
-        echo -e "${GREEN}[*] ${RESET}Base setup is now complete, goodbye!"
-        echo -e "${GREEN}[*] (Time: $(( $(( FINISH_TIME - START_TIME )) / 60 )) minutes)${RESET}"
+  FINISH_TIME=$(date +%s)
+  echo -e "${GREEN}[*] ${RESET}Base setup is now complete, goodbye!"
+  echo -e "${GREEN}[*] (Time: $(( $(( FINISH_TIME - START_TIME )) / 60 )) minutes)${RESET}"
 }
 trap finish EXIT
 
 
-# -==========================-[ Misc Notes ]-==========================-#
+# -==========================-[ Misc Debian Notes ]-==========================-#
 ### How to turn off IPv6
 
 # Append ipv6.disable=1 to the GRUB_CMDLINE_LINUX variable in /etc/default/grub.
